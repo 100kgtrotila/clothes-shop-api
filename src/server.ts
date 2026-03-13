51dee295456ae212864f614be908a1fe367b1412
Danylo Marynych <hirodevi192@gmail.com>
2026-03-13 15:49:21 +0200

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
 import express, { type Request, type Response } from 'express';
+import { clerkMiddleware } from '@clerk/express'
+import categoryRoutes from './features/category/category.routes.js';
+import { errorHandler } from './middlewares/error.middleware.js';
 
 const app = express()
 const PORT = 3000;
 
 app.use(express.json());
+
+app.use(clerkMiddleware());
+
+app.use('/api/categories', categoryRoutes);
+
+app.use(errorHandler)
+
 
 app.listen(PORT, () => {
     console.log(`Server running on localhost:${PORT}`)
