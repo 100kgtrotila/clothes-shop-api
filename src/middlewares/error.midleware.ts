2c5d51352aa769f41c25c3f24437ce8e2c15fc4f
Danylo Marynych <hirodevi192@gmail.com>
2026-03-13 13:41:52 +0200

    Add global error handler middleware
    Handle Prisma P2002 duplicate errors by returning 409 and log the error.
    Return JSON with success=false and message; include stack only in development.
    Provide Ukrainian fallback for unspecified errors.

---
+++ src/middlewares/error.midleware.ts
@@ -0,0 +1,24 @@
+import type { NextFunction, Request, Response } from "express";
+import { Prisma } from "../generated/client.js";
+
+export const errorHandler = (err: any, req: Request, res: Response, next: NextFunction): void => {
+    console.log('Global Error', err.message || err);
+
+    if (err instanceof Prisma.PrismaClientKnownRequestError) {
+        if (err.code === 'P2002') {
+            res.status(409).json({
+                success: false,
+                message: "The record is already exists in db"
+            });
+            return;
+        }
+    }
+
+
+    const statusCode = err.status || 500;
+      res.status(statusCode).json({
+        success: false,
+        message: err.message || 'Внутрішня помилка сервера',
+        stack: process.env.NODE_ENV === 'development' ? err.stack : undefined,
+      });
+};
