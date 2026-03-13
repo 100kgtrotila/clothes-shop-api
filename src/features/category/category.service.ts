90f2c5325be666a33d332bd4b0411c08e71d674f
Danylo Marynych <hirodevi192@gmail.com>
2026-03-13 13:09:04 +0200

    Add Zod dependency and fix formatting

--- src/features/category/category.service.ts
+++ src/features/category/category.service.ts
@@ -5,20 +5,18 @@ export class CategoryService {
     async getAll() {
         return prisma.category.findMany();
     }
-    
+
     async create(data: createCategoryInput) {
         const exists = await prisma.category.findUnique({
             where: { slug: data.slug }
         });
-        
+
         if (exists) {
             throw new Error(`Category with slug ${data.slug} is already exists`);
         }
-        
+
         return prisma.category.create({ data });
     }
 }
 
 export const categoryService = new CategoryService();
-
-
