b4ccf7953043a1b12fba740c207940ddc37c1756
Danylo Marynych <hirodevi192@gmail.com>
2026-03-13 13:08:14 +0200

    Create category.controller.ts

---
+++ src/features/category/category.controller.ts
@@ -0,0 +1,16 @@
+import type { Request, Response } from 'express';
+import { categoryService } from './category.service.js';
+
+export class CategoryController {
+    async getAllCategories(req: Request, res: Response) {
+        const categories = await categoryService.getAll();
+        res.status(200).json({
+            success: true, data: categories
+        });
+    }
+
+    async createCategory(req: Request, res: Response) {
+        const newCategory = await categoryService.create(req.body);
+        res.status(201).json({ success: true, data: newCategory });
+    }
+}
