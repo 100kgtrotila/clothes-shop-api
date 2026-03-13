bc35ee17a7fc9b14f76376fcadfcdb4ff51c058c
Danylo Marynych <hirodevi192@gmail.com>
2026-03-13 13:07:14 +0200

    Add createCategory Zod schema

---
+++ src/features/category/category.schema.ts
@@ -0,0 +1,10 @@
+import { z } from 'zod';
+
+export const createCategorySchema = z.object({
+    body: z.object({
+        name: z.string().min(2),
+        slug: z.string().regex(/^[a-z0-9-]+s/)
+    })
+});
+
+export type createCategoryInput = z.infer<typeof createCategorySchema>['body'];
