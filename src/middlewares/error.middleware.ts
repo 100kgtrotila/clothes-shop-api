e603fcde6f916f37602b6344d868ef702f5c5fe1
Danylo Marynych <hirodevi192@gmail.com>
2026-03-14 13:21:02 +0200

    Format error middleware and mark unused params

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,24 +1,28 @@
 import type { NextFunction, Request, Response } from "express";
 import { Prisma } from "../generated/client.js";
 
-export const errorHandler = (err: any, req: Request, res: Response, next: NextFunction): void => {
-    console.log('Global Error', err.message || err);
+export const errorHandler = (
+	err: any,
+	_req: Request,
+	res: Response,
+	_next: NextFunction,
+): void => {
+	console.log("Global Error", err.message || err);
 
-    if (err instanceof Prisma.PrismaClientKnownRequestError) {
-        if (err.code === 'P2002') {
-            res.status(409).json({
-                success: false,
-                message: "The record is already exists in db"
-            });
-            return;
-        }
-    }
+	if (err instanceof Prisma.PrismaClientKnownRequestError) {
+		if (err.code === "P2002") {
+			res.status(409).json({
+				success: false,
+				message: "The record is already exists in db",
+			});
+			return;
+		}
+	}
 
-
-    const statusCode = err.status || 500;
-      res.status(statusCode).json({
-        success: false,
-        message: err.message || 'Внутрішня помилка сервера',
-        stack: process.env.NODE_ENV === 'development' ? err.stack : undefined,
-      });
+	const statusCode = err.status || 500;
+	res.status(statusCode).json({
+		success: false,
+		message: err.message || "Внутрішня помилка сервера",
+		stack: process.env.NODE_ENV === "development" ? err.stack : undefined,
+	});
 };
