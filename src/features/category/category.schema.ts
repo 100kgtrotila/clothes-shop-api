f791b5f4529708f8320a78e55a0fd50bd665fad0
Danylo Marynych <hirodevi192@gmail.com>
2026-03-14 11:33:10 +0200

    Fix category slug regex

--- src/features/category/category.schema.ts
+++ src/features/category/category.schema.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 export const createCategorySchema = z.object({
     body: z.object({
         name: z.string().min(2),
-        slug: z.string().regex(/^[a-z0-9-]+s/)
+        slug: z.string().regex(/^[a-z0-9-]+$/)
     })
 });
 
