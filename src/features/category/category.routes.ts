14a16d4fed6827bd5e6ea738c8e3324c4c6c48e0
Danylo Marynych <hirodevi192@gmail.com>
2026-03-15 22:55:44 +0200

    Require API auth for category creation

--- src/features/category/category.routes.ts
+++ src/features/category/category.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { createCategorySchema } from "./category.schema.js";
 import { validate } from "../../middlewares/validate.js";
-
+import { requireApiAuth } from "../../middlewares/auth.middleware.js";
 import categoryController from "./category.controller.js";
 
 const router = Router();
@@ -9,6 +9,7 @@ const router = Router();
 router.get("/", categoryController.getAllCategories);
 router.post(
 	"/",
+	requireApiAuth,
 	validate(createCategorySchema),
 	categoryController.createCategory,
 );
