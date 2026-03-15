ebf301394c9c7e8c162132ed5bc97c7e73daca7c
Danylo Marynych <hirodevi192@gmail.com>
2026-03-15 22:55:28 +0200

    Register user routes and add test endpoints

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,23 @@ import express from "express";
 import { clerkMiddleware } from "@clerk/express";
 import categoryRoutes from "./features/category/category.routes.js";
 import { errorHandler } from "./middlewares/error.middleware.js";
+import userRoutes from "./features/user/user.routes.js";
 
 const app = express();
+
 const PORT = 3000;
+
+app.use(clerkMiddleware());
+app.use("/api/users", userRoutes);
 
 app.use(express.json());
 
-app.use(clerkMiddleware());
+app.post("/api/test", (req, res) => {
+	res.json({ message: "Бекенд живий і бачить POST запит!" });
+});
+app.get("/api/test", (req, res) => {
+	res.json({ message: "Бекенд живий і бачить GET запит!" });
+});
 
 app.use("/api/categories", categoryRoutes);
 
