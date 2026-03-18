a003613cbfd3bd59f87e5f3502009c6b3305ac7e
Danylo Marynych <hirodevi192@gmail.com>
2026-03-18 11:06:45 +0200

    Normalize quotes/indentation in category schema

--- src/features/category/category.schema.ts
+++ src/features/category/category.schema.ts
@@ -1,10 +1,10 @@
-import { z } from 'zod';
+import { z } from "zod";
 
 export const createCategorySchema = z.object({
-    body: z.object({
-        name: z.string().min(2),
-        slug: z.string().regex(/^[a-z0-9-]+$/)
-    })
+	body: z.object({
+		name: z.string().min(2),
+		slug: z.string().regex(/^[a-z0-9-]+$/),
+	}),
 });
 
-export type createCategoryInput = z.infer<typeof createCategorySchema>['body'];
+export type createCategoryInput = z.infer<typeof createCategorySchema>["body"];
