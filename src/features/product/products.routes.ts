3f6e0b0ed75d795382f1603d151d20fff45dc0e7
Danylo Marynych <hirodevi192@gmail.com>
2026-03-18 11:02:16 +0200

    Add product routes with auth and validation
    Create src/features/product/products.routes.ts defining GET / and POST /
    endpoints. Protect POST with requireApiAuth and validate using
    createProductSchema.

---
+++ src/features/product/products.routes.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import { requireApiAuth } from "../../middlewares/auth.middleware.js";
+import { validate } from "../../middlewares/validate.js";
+import productController from "./product,controller.js";
+import { createProductSchema } from "./product.schema.js";
+
+const router = Router();
+
+router.get("/", productController.getAllProducts);
+router.post(
+	"/",
+	requireApiAuth,
+	validate(createProductSchema),
+	productController.createProduct,
+);
