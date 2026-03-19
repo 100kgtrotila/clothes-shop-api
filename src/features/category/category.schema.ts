3f9d6e259b3460ca068b7a36d9c954e9d4d67e15
Danylo Marynych <hirodevi192@gmail.com>
2026-03-19 20:15:03 +0200

    Add update/delete category schemas and DTOs

--- src/features/category/category.schema.ts
+++ src/features/category/category.schema.ts
@@ -7,4 +7,16 @@ export const createCategorySchema = z.object({
 	}),
 });
 
-export type createCategoryInput = z.infer<typeof createCategorySchema>["body"];
+export const updateCategorySchema = z.object({
+	body: createCategorySchema.shape.body.partial(),
+});
+
+export const deleteCategorySchema = z.object({
+	params: z.object({
+		id: z.uuid(),
+	}),
+});
+
+export type createCategoryDto = z.infer<typeof createCategorySchema>["body"];
+export type updateCategoryDto = z.infer<typeof updateCategorySchema>["body"];
+export type deleteCategoryDto = z.infer<typeof deleteCategorySchema>["params"];
