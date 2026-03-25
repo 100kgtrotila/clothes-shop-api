cdafe37e1f35e7835a252706347a8fe5a1494d71
100kgtrotila <hirodevi192@gmail.com>
2026-03-25 17:41:51 +0200

    feat(order): add getMyOrders endpoint to order controller
    Introduce a new endpoint to fetch user orders with query parameters.
    This enhances the order management functionality.

--- src/features/order/order.controller.ts
+++ src/features/order/order.controller.ts
@@ -1,19 +1,25 @@
 import type { Request, Response } from "express";
 import orderService from "./order.service.js";
-import { getAuth } from "node_modules/@clerk/express/dist/index.js";
+import { getMyOrdersSchema, type GetMyOrdersDto } from "./order.schema.js";
 
 export class OrderController {
 	async cheokoutCart(req: Request, res: Response) {
 		const userId = req.user.id;
-
-		if (!userId) {
-			res.status(401).json({ success: false, message: "Unauthorized" });
-			return;
-		}
-
 		const order = await orderService.checkout(userId);
 		res.status(201).json({
 			status: true,
+			data: order,
+		});
+	}
+
+	async getMyOrders(req: Request, res: Response) {
+		const userId = req.user.id;
+
+		const query = req.query as unknown as GetMyOrdersDto;
+
+		const order = await orderService.myOrders(userId, query);
+		res.status(200).json({
+			success: true,
 			data: order,
 		});
 	}
