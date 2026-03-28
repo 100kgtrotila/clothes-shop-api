aadd9c2e92092317b8ae3511010ec1632d875f80
100kgtrotila <hirodevi192@gmail.com>
2026-03-28 18:07:29 +0200

    feat(order): add route to get order by ID
    Add a new GET route to fetch an order by its ID. This route
    requires authentication and validates the request using the
    `getOrderByIdSchema`.

--- src/features/order/order.routes.ts
+++ src/features/order/order.routes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import orderController from "./order.controller.js";
 import { requireApiAuth } from "@/middlewares/auth.middleware.js";
 import { validate } from "@/middlewares/validate.js";
-import { getMyOrdersSchema } from "./order.schema.js";
+import { getMyOrdersSchema, getOrderByIdSchema } from "./order.schema.js";
 
 const router = Router();
 
@@ -13,5 +13,10 @@ router.get(
 	orderController.getMyOrders,
 );
 router.post("/checkout", requireApiAuth, orderController.cheokoutCart);
-
+router.get(
+	"/:id",
+	requireApiAuth,
+	validate(getOrderByIdSchema),
+	orderController.getOrderById,
+);
 export default router;
