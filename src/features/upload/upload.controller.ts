00d9ad0dcba7d52f9d879b650e556a22a9758de2
100kgtrotila <hirodevi192@gmail.com>
2026-03-31 22:11:39 +0300

    feat(upload): add upload controller for image handling
    Introduce `UploadController` with `uploadImage` method to handle
    image uploads. Validates file presence and uploads to S3, returning
    the image URL in the response.

---
+++ src/features/upload/upload.controller.ts
@@ -0,0 +1,27 @@
+import type { Request, Response } from "express";
+import { AppError } from "../../errors/app.error.js";
+import { uploadFileToS3 } from "./upload.service.js";
+import multer from "multer";
+
+export class UploadController {
+	async uploadImage(req: Request, res: Response) {
+		if (!req.file) {
+			throw new AppError("No file uploaded", 400);
+		}
+
+		const imageUrl = await uploadFileToS3(
+			req.file.buffer,
+			req.file.originalname,
+			req.file.mimetype,
+		);
+
+		res.status(200).json({
+			success: true,
+			data: {
+				url: imageUrl,
+			},
+		});
+	}
+}
+
+export default new UploadController();
