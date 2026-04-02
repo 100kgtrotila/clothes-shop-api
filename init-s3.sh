197528ab84bd2f8a4df18c84a2cf7a605c00bb31
100kgtrotila <hirodevi192@gmail.com>
2026-04-02 14:40:56 +0300

    feat: add check for existing S3 bucket before creation
    Ensure the script checks if the S3 bucket already exists before
    attempting to create it, avoiding redundant operations.

--- init-s3.sh
+++ init-s3.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 echo "Loading LocalStack S3..."
 
-awslocal s3 mb s3://clothes-shop-images
-
-awslocal s3api put-bucket-acl --bucket clothes-shop-images --acl public-read
-
-echo "bucket clothes-shop-images is enabled!"
+if awslocal s3api head-bucket --bucket clothes-shop-images 2>/dev/null; then
+  echo "Bucket clothes-shop-images already exists. Skipping creation."
+else
+  echo "Bucket not found. Creating..."
+  awslocal s3 mb s3://clothes-shop-images
+  awslocal s3api put-bucket-acl --bucket clothes-shop-images --acl public-read
+  echo "Bucket clothes-shop-images is created and enabled!"
+fi
