44c77fd2e6d98e9935d5da08c095fc056041f42f
100kgtrotila <hirodevi192@gmail.com>
2026-04-04 13:56:09 +0300

    feat: add outbox worker for event processing
    Introduce an outbox worker to process unprocessed events from the
    database and push them to a RabbitMQ queue. This worker runs at
    regular intervals and ensures reliable event delivery.

---
+++ src/workers/outobox.worker.ts
@@ -0,0 +1,40 @@
+import { prisma } from "../db/prisma.js";
+import amqp from "amqplib";
+import { logger } from "../utils/logger.js";
+
+async function startOutboxWorker() {
+	const connection = await amqp.connect(
+		process.env.RABBITMQ_URL || "amqp://localhost",
+	);
+
+	const channel = await connection.createChannel();
+	const queue = "order_notifications";
+
+	await channel.assertQueue(queue, { durable: true });
+
+	logger.info("Outbox worker  started");
+
+	setInterval(async () => {
+		const events = await prisma.outboxEvent.findMany({
+			where: { processed: false },
+			take: 10,
+		});
+
+		for (const event of events) {
+			try {
+				const messsge = JSON.stringify(event.payload);
+				channel.sendToQueue(queue, Buffer.from(messsge), { persistent: true });
+				await prisma.outboxEvent.update({
+					where: { id: event.id },
+					data: { processed: true },
+				});
+
+				logger.info({ eventId: event.id }, "Event pushed to RabbitMQ");
+			} catch (err) {
+				logger.error({ err }, "Failed to push event to RabbitMQ");
+			}
+		}
+	}, 5000);
+}
+
+startOutboxWorker();
