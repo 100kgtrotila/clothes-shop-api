05671ff1da2f845814cdd62f55e90122cca554e1
100kgtrotila <hirodevi192@gmail.com>
2026-04-08 11:28:04 +0300

    feat(email): generate dynamic receipt email content
    Replace static HTML email

--- src/constumers/email.consumer.ts
+++ src/constumers/email.consumer.ts
@@ -1,6 +1,8 @@
 import amqp from "amqplib";
 import { Resend } from "resend";
 import { logger } from "@/utils/logger.js";
+import { render } from "@react-email/render";
+import { ReceiptEmail } from "../templates/ReceiptEmail.js";
 
 const resend = new Resend(process.env.RESEND_API_KEY!);
 
@@ -18,25 +20,27 @@ export async function startEmailConsumer() {
 		if (msg !== null) {
 			try {
 				const payload = JSON.parse(msg.content.toString());
-				const { orderId, customerEmail, amount } = payload;
+				const { orderId, customerEmail, amount, items } = payload;
 
 				logger.info({ orderId }, "Consumer grabbed a message");
+
+				const htmlContent = await render(
+					ReceiptEmail({
+						orderId: orderId,
+						items: items,
+						subtotal: amount / 100,
+						shipping: 0,
+						total: amount / 100,
+						customerName: customerEmail.split("@")[0],
+						address: "Shipping Address",
+					}),
+				);
 
 				const { data, error } = await resend.emails.send({
 					from: "Grain Shop by holydxvi <noreply@holydxvi.me>",
 					to: [customerEmail],
-					subject: "Successful purchase",
-					html: `
-						<div style="font-family: sans-serif; padding: 20px; color: #333;">
-							<h2 style="color: #F5A623;">Thank you for your order!</h2>
-							<p>We have successfully received your payment. Your order is confirmed.</p>
-							<div style="background: #f9f9f9; padding: 15px; border-radius: 8px; margin: 20px 0;">
-								<p><strong>Order ID:</strong> ${orderId}</p>
-								<p><strong>Total Amount:</strong> ${(amount / 100).toFixed(2)} UAH</p>
-							</div>
-							<a href="http://localhost:5173/orders" style="background: #333; color: white; padding: 10px 20px; text-decoration: none; border-radius: 5px; display: inline-block;">View Order Status</a>
-						</div>
-					`,
+					subject: `Order Confirmation #${orderId.substring(0, 8)}`,
+					html: htmlContent,
 				});
 
 				if (error) {
